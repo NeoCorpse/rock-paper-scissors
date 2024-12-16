abe3f84f40afb15507e6c14a7ae9c2c662c60e00
Ian <neocorpse@proton.me>
2024-12-16 08:56:55 +0300

    Add functions for getting computer and user choices

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-"use strict"
+"use strict";
+
+let humanScore;
+let computerScore;
+let rounds;
+
+function getCompChoice() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * 3)]
+}
+
+function getUserChoice() {
+    return (prompt('What do you pick? Rock, paper or scissors?')).toLowerCase()
+}
