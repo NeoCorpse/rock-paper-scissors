a9aad71cd8a9bf73c9f20f8efad837c59ab62cd2
Ian <neocorpse@proton.me>
2024-12-16 09:18:32 +0300

    Add and test function for playing a single round

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let humanScore;
-let computerScore;
+let humanScore = 0;
+let computerScore = 0;
 let rounds;
 
 function getCompChoice() {
@@ -11,4 +11,59 @@ function getCompChoice() {
 
 function getUserChoice() {
     return (prompt('What do you pick? Rock, paper or scissors?')).toLowerCase()
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock') {
+        switch(computerChoice) {
+            case 'rock':
+                alert(`Player: ${humanChoice}  Computer: ${computerChoice} \nThat's a tie!`)
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+                break;
+            case 'paper':
+                alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nShoot! Paper beats rock :(`)
+                computerScore++;
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+                break;
+            case 'scissors': 
+            alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nYay! Rock beats scissors :)`)
+                humanScore++;
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+                break;
+        }
+    } else if (humanChoice === 'paper') {
+        switch(computerChoice) {
+            case 'paper':
+                alert(`Player: ${humanChoice} || Computer: ${computerChoice}
+                    That's a tie!`)
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+            case 'rock':
+                alert(`Player: ${humanChoice} || Computer: ${computerChoice}
+                    Yay! Paper beats rock :)`)
+                computerScore++;
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+            case 'scissors': 
+            alert(`Player: ${humanChoice} || Computer: ${computerChoice}
+                Shoot! Scissors beats paper :(`)
+                humanScore++;
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+        }
+    } else if (humanChoice === 'scissors'){
+        switch(computerChoice) {
+            case 'scissors':
+                alert(`Player: ${humanChoice} || Computer: ${computerChoice}
+                    That's a tie!`)
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+            case 'rock':
+                alert(`Player: ${humanChoice} || Computer: ${computerChoice}
+                    Shoot! Rock beats scissors :(`)
+                computerScore++;
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+            case 'paper': 
+            alert(`Player: ${humanChoice} || Computer: ${computerChoice}
+                Yay! Scissors beats paper :)`)
+                humanScore++;
+                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
+        }
+    }
 }
