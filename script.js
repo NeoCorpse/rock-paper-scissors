86af9a51d774fad0cd5fd5034ed21132357ce936
Ian <neocorpse@proton.me>
2024-12-20 16:07:34 +0300

    Change interactivity from console to DOM
    I had to refactor the code to work with event listeners and used
    unstyled html elements

--- script.js
+++ script.js
@@ -1,88 +1,105 @@
-"use strict";
+'use strict';
 
 let humanScore = 0;
 let computerScore = 0;
 let rounds = 1;
-let choices = ['rock', 'paper', 'scissors']
+let choices = ['rock', 'paper', 'scissors'];
+const roundPara = document.querySelector('.rounds');
+const result = document.querySelector('div.result');
+const buttons = document.querySelector('.buttons');
+const scores = document.querySelector('.scores');
+
+roundPara.textContent = `Round ${rounds}. First to five wins!`;
+
+function playGame(e, computerChoice = getCompChoice()) {
+	let choice = e.target.textContent.toLowerCase();
+	if (choice === 'rock') {
+		switch (computerChoice) {
+			case 'rock':
+				result.textContent = "That's a tie!";
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+			case 'paper':
+				result.textContent = 'Shoot! Paper beats rock :(';
+				computerScore++;
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+			case 'scissors':
+				result.textContent = 'Yay! Rock beats scissors :)';
+				humanScore++;
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+		}
+	}
+	if (choice === 'paper') {
+		switch (computerChoice) {
+			case 'paper':
+				result.textContent = "That's a tie!";
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+			case 'scissors':
+				result.textContent = 'Shoot! Scissors beats paper :(';
+				computerScore++;
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+			case 'rock':
+				result.textContent = 'Yay! Paper beats rock :)';
+				humanScore++;
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+		}
+	}
+	if (choice === 'scissors') {
+		switch (computerChoice) {
+			case 'scissors':
+				result.textContent = "That's a tie!";
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+			case 'rock':
+				result.textContent = 'Shoot! Rock beats scissors :(';
+				computerScore++;
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+			case 'paper':
+				result.textContent = 'Yay! Scissors beats paper :)';
+				humanScore++;
+				scores.textContent = `Player score: ${humanScore} || Computer score: ${computerScore}`;
+				break;
+		}
+	}
+	rounds++;
+	if (humanScore === 5) {
+		result.textContent = 'Congratulations!! You won!!!';
+		endGame();
+	} else if (computerScore === 5) {
+		result.textContent = 'You lost!! Womp Womp :(';
+		endGame();
+	}
+}
+
+buttons.addEventListener('click', playGame);
+
+function endGame() {
+	buttons.removeEventListener('click', playGame);
+	const para = document.createElement('p');
+	para.textContent = 'Do you want to play again';
+	const myDiv = document.createElement('div');
+	const myButton = document.createElement('button');
+	myButton.textContent = 'Restart';
+	myButton.addEventListener('click', (e) => {
+		humanScore = 0;
+		computerScore = 0;
+		rounds = 1;
+		result.textContent = '';
+		scores.textContent = '';
+		document.body.removeChild(myDiv);
+		buttons.addEventListener('click', playGame);
+	});
+	myDiv.appendChild(para);
+	myDiv.appendChild(myButton);
+	document.body.appendChild(myDiv);
+}
 
 function getCompChoice() {
-    return choices[Math.floor(Math.random() * 3)]
-}
-
-function getUserChoice() {
-    alert(`Round ${rounds}!`)
-    return (prompt('What do you pick? Rock, paper or scissors?')).toLowerCase()
-
-}
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === 'rock') {
-        switch(computerChoice) {
-            case 'rock':
-                alert(`Player: ${humanChoice}  Computer: ${computerChoice} \nThat's a tie!`)
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-            case 'paper':
-                alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nShoot! Paper beats rock :(`)
-                computerScore++;
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-            case 'scissors': 
-            alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nYay! Rock beats scissors :)`)
-                humanScore++;
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-        }
-    } else if (humanChoice === 'paper') {
-        switch(computerChoice) {
-            case 'paper':
-                alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nThat's a tie!`)
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-            case 'rock':
-                alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nYay! Paper beats rock :)`)
-                computerScore++;
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-            case 'scissors': 
-                alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nShoot! Scissors beats paper :(`)
-                humanScore++;
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-        }
-    } else if (humanChoice === 'scissors'){
-        switch(computerChoice) {
-            case 'scissors':
-                alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nThat's a tie!`)
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-            case 'rock':
-                alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nShoot! Rock beats scissors :(`)
-                computerScore++;
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-            case 'paper': 
-            alert(`Player: ${humanChoice} || Computer: ${computerChoice} \nYay! Scissors beats paper :)`)
-                humanScore++;
-                alert(`Player score: ${humanScore} || Computer score: ${computerScore}`)
-                break;
-        }
-    }
-}
-
-function playGame() {
-    humanScore = 0;
-    computerScore = 0;
-    rounds = 1;
-    if (confirm('Are you ready to play?')) {
-        while (rounds <= 5){
-            playRound(getUserChoice(), getCompChoice())
-            rounds++;
-        }
-
-        humanScore > computerScore? console.log('Yay! You won!') : humanScore === computerScore ? console.log('You tied! Try again to defeat me!'): console.log('Noooo! You lost!');
-        console.log(`Your score: ${humanScore}. Computer's score ${computerScore}.`)
-    } else {
-        alert('I knew you were too scared to face me.')
-    }
+	return choices[Math.floor(Math.random() * 3)];
 }
